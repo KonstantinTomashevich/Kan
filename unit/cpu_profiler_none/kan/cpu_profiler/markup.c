3d07f0afac0f5c873bc13f2ff71bb92d578b9ebf
KonstantinTomashevich <konstantin.tomashevich@gmail.com>
2023-11-14 20:39:08 +0300

    #5. cpu_profiler: Make sections global and make it possible to query sections.

--- unit/cpu_profiler_none/kan/cpu_profiler/markup.c
+++ unit/cpu_profiler_none/kan/cpu_profiler/markup.c
@@ -4,15 +4,16 @@ void kan_cpu_stage_separator (void)
 {
 }
 
-void kan_cpu_section_init (struct kan_cpu_section_t *section, const char *name, uint32_t rgba_color)
+kan_cpu_section_t kan_cpu_section_get (const char *name)
+{
+    return 0u;
+}
+
+void kan_cpu_section_set_color (kan_cpu_section_t section, uint32_t rgba_color)
 {
 }
 
-void kan_cpu_section_destroy (struct kan_cpu_section_t *section)
-{
-}
-
-void kan_cpu_section_execution_init (struct kan_cpu_section_execution_t *execution, struct kan_cpu_section_t *section)
+void kan_cpu_section_execution_init (struct kan_cpu_section_execution_t *execution, kan_cpu_section_t section)
 {
 }
 
