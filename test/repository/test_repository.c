1446cc76ac698db39736e1a893c8bce9bb7aa46b
KonstantinTomashevich <konstantin.tomashevich@gmail.com>
2023-12-04 20:06:17 +0300

    #5. repository: Add tests for singleton including observation events.

--- test/repository/test_repository.c
+++ test/repository/test_repository.c
@@ -1,13 +1,141 @@
+#include <test_repository_api.h>
+
+#include <stddef.h>
+
 #include <kan/container/stack_group_allocator.h>
 #include <kan/cpu_dispatch/job.h>
 #include <kan/reflection/generated_reflection.h>
 #include <kan/repository/repository.h>
 #include <kan/testing/testing.h>
 
-#include "test_repository.h"
-
 KAN_REFLECTION_EXPECT_UNIT_REGISTRAR (repository);
 KAN_REFLECTION_EXPECT_UNIT_REGISTRAR (test_repository);
+
+struct manual_event_t
+{
+    int32_t x;
+    int32_t y;
+};
+
+struct manual_event_second_t
+{
+    uint64_t id;
+    float a;
+    float b;
+};
+
+struct first_singleton_t
+{
+    uint32_t x;
+    uint32_t y;
+    uint32_t observable_z;
+
+    kan_interned_string_t some_string;
+    float observable_a;
+    float observable_b;
+};
+
+TEST_REPOSITORY_API void first_singleton_init (uint64_t reflection_user_data, void *data)
+{
+    *(struct first_singleton_t *) data = (struct first_singleton_t) {
+        .x = 0u,
+        .y = 0u,
+        .observable_z = 0u,
+
+        .some_string = kan_string_intern ("some_value"),
+        .observable_a = 0.0f,
+        .observable_b = 0.0f,
+    };
+}
+
+struct first_singleton_z_changed_event_t
+{
+    uint32_t old_z;
+    uint32_t new_z;
+};
+
+// \meta reflection_struct_meta = "first_singleton_t"
+TEST_REPOSITORY_API struct kan_repository_meta_automatic_on_change_event_t first_singleton_z_changed_event = {
+    .event_type = "first_singleton_z_changed_event_t",
+    .observed_fields_count = 1u,
+    .observed_fields =
+        (struct kan_repository_field_path_t[]) {
+            {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"observable_z"}},
+        },
+    .unchanged_copy_outs_count = 1u,
+    .unchanged_copy_outs =
+        (struct kan_repository_copy_out_t[]) {
+            {
+                .source_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"observable_z"}},
+                .target_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"old_z"}},
+            },
+        },
+    .changed_copy_outs_count = 1u,
+    .changed_copy_outs =
+        (struct kan_repository_copy_out_t[]) {
+            {
+                .source_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"observable_z"}},
+                .target_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"new_z"}},
+            },
+        },
+};
+
+struct first_singleton_a_b_changed_event_t
+{
+    float old_a;
+    float old_b;
+    float new_a;
+    float new_b;
+};
+
+// \meta reflection_struct_meta = "first_singleton_t"
+TEST_REPOSITORY_API struct kan_repository_meta_automatic_on_change_event_t first_singleton_a_b_changed_event = {
+    .event_type = "first_singleton_a_b_changed_event_t",
+    .observed_fields_count = 2u,
+    .observed_fields =
+        (struct kan_repository_field_path_t[]) {
+            {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"observable_a"}},
+            {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"observable_b"}},
+        },
+    .unchanged_copy_outs_count = 2u,
+    .unchanged_copy_outs =
+        (struct kan_repository_copy_out_t[]) {
+            {
+                .source_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"observable_a"}},
+                .target_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"old_a"}},
+            },
+            {
+                .source_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"observable_b"}},
+                .target_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"old_b"}},
+            },
+        },
+    .changed_copy_outs_count = 2u,
+    .changed_copy_outs =
+        (struct kan_repository_copy_out_t[]) {
+            {
+                .source_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"observable_a"}},
+                .target_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"new_a"}},
+            },
+            {
+                .source_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"observable_b"}},
+                .target_path = {.reflection_path_length = 1u, .reflection_path = (const char *[]) {"new_b"}},
+            },
+        },
+};
+
+struct second_singleton_t
+{
+    uint32_t a;
+    uint32_t b;
+};
+
+TEST_REPOSITORY_API void second_singleton_init (uint64_t reflection_user_data, void *data)
+{
+    *(struct second_singleton_t *) data = (struct second_singleton_t) {
+        .a = 0u,
+        .b = 0u,
+    };
+}
 
 static void check_no_event (struct kan_repository_event_fetch_query_t *query)
 {
@@ -74,7 +202,7 @@ static void check_manual_event_second (struct kan_repository_event_fetch_query_t
     kan_repository_event_read_access_close (&access);
 }
 
-struct check_manual_event_from_task_user_data
+struct check_manual_event_from_task_user_data_t
 {
     struct kan_repository_event_read_access_t access;
     struct manual_event_t data;
@@ -82,8 +210,8 @@ struct check_manual_event_from_task_user_data
 
 static void check_manual_event_from_task_executor (uint64_t user_data_int)
 {
-    struct check_manual_event_from_task_user_data *user_data =
-        (struct check_manual_event_from_task_user_data *) user_data_int;
+    struct check_manual_event_from_task_user_data_t *user_data =
+        (struct check_manual_event_from_task_user_data_t *) user_data_int;
 
     struct manual_event_t *event =
         (struct manual_event_t *) kan_repository_event_read_access_resolve (&user_data->access);
@@ -100,9 +228,9 @@ static void check_manual_event_from_task (struct kan_repository_event_fetch_quer
                                           struct manual_event_t data)
 {
     struct kan_repository_event_read_access_t access = kan_repository_event_fetch_query_next (query);
-    struct check_manual_event_from_task_user_data *user_data =
-        kan_stack_group_allocator_allocate (temporary_allocator, sizeof (struct check_manual_event_from_task_user_data),
-                                            _Alignof (struct check_manual_event_from_task_user_data));
+    struct check_manual_event_from_task_user_data_t *user_data = kan_stack_group_allocator_allocate (
+        temporary_allocator, sizeof (struct check_manual_event_from_task_user_data_t),
+        _Alignof (struct check_manual_event_from_task_user_data_t));
 
     user_data->access = access;
     user_data->data = data;
@@ -119,6 +247,34 @@ static void check_manual_event_from_task (struct kan_repository_event_fetch_quer
     };
 
     task_node->queue = KAN_CPU_DISPATCH_QUEUE_FOREGROUND;
+}
+
+static void check_z_changed_event (struct kan_repository_event_fetch_query_t *query,
+                                   struct first_singleton_z_changed_event_t data)
+{
+    struct kan_repository_event_read_access_t access = kan_repository_event_fetch_query_next (query);
+    struct first_singleton_z_changed_event_t *event =
+        (struct first_singleton_z_changed_event_t *) kan_repository_event_read_access_resolve (&access);
+
+    KAN_TEST_ASSERT (event)
+    KAN_TEST_CHECK (event->old_z == data.old_z)
+    KAN_TEST_CHECK (event->new_z == data.new_z)
+    kan_repository_event_read_access_close (&access);
+}
+
+static void check_a_b_changed_event (struct kan_repository_event_fetch_query_t *query,
+                                     struct first_singleton_a_b_changed_event_t data)
+{
+    struct kan_repository_event_read_access_t access = kan_repository_event_fetch_query_next (query);
+    struct first_singleton_a_b_changed_event_t *event =
+        (struct first_singleton_a_b_changed_event_t *) kan_repository_event_read_access_resolve (&access);
+
+    KAN_TEST_ASSERT (event)
+    KAN_TEST_CHECK (event->old_a == data.old_a)
+    KAN_TEST_CHECK (event->old_b == data.old_b)
+    KAN_TEST_CHECK (event->new_a == data.new_a)
+    KAN_TEST_CHECK (event->new_b == data.new_b)
+    kan_repository_event_read_access_close (&access);
 }
 
 KAN_TEST_CASE (manual_event)
@@ -327,6 +483,240 @@ KAN_TEST_CASE (manual_event_access_from_tasks)
     kan_repository_event_insert_query_shutdown (&event_insert);
     kan_repository_event_fetch_query_shutdown (&event_fetch_first);
     kan_repository_event_fetch_query_shutdown (&event_fetch_second);
+
+    kan_repository_destroy (repository);
+    kan_reflection_registry_destroy (registry);
+}
+
+KAN_TEST_CASE (singleton_access)
+{
+    kan_reflection_registry_t registry = kan_reflection_registry_create ();
+    KAN_REFLECTION_UNIT_REGISTRAR_NAME (repository) (registry);
+    KAN_REFLECTION_UNIT_REGISTRAR_NAME (test_repository) (registry);
+
+    kan_repository_t root_repository = kan_repository_create_root (KAN_ALLOCATION_GROUP_IGNORE, registry);
+    kan_repository_t child_repository = kan_repository_create_child (root_repository, "child");
+
+    kan_repository_singleton_storage_t first_storage_root =
+        kan_repository_singleton_storage_open (root_repository, "first_singleton_t");
+
+    kan_repository_singleton_storage_t first_storage_child =
+        kan_repository_singleton_storage_open (child_repository, "first_singleton_t");
+
+    kan_repository_singleton_storage_t second_storage_child =
+        kan_repository_singleton_storage_open (child_repository, "second_singleton_t");
+
+    struct kan_repository_singleton_read_query_t first_read_from_root;
+    kan_repository_singleton_read_query_init (&first_read_from_root, first_storage_root);
+
+    struct kan_repository_singleton_write_query_t first_write_from_child;
+    kan_repository_singleton_write_query_init (&first_write_from_child, first_storage_child);
+
+    struct kan_repository_singleton_write_query_t second_write_from_child;
+    kan_repository_singleton_write_query_init (&second_write_from_child, second_storage_child);
+
+    kan_repository_enter_serving_mode (root_repository);
+
+    {
+        kan_repository_singleton_read_access_t first_access =
+            kan_repository_singleton_read_query_execute (&first_read_from_root);
+
+        const struct first_singleton_t *singleton =
+            (struct first_singleton_t *) kan_repository_singleton_read_access_resolve (first_access);
+
+        KAN_TEST_ASSERT (singleton)
+        KAN_TEST_CHECK (singleton->x == 0u)
+        KAN_TEST_CHECK (singleton->y == 0u)
+        KAN_TEST_CHECK (singleton->observable_z == 0u)
+
+        KAN_TEST_CHECK (singleton->some_string == kan_string_intern ("some_value"))
+        KAN_TEST_CHECK (singleton->observable_a == 0.0f)
+        KAN_TEST_CHECK (singleton->observable_b == 0.0f)
+
+        kan_repository_singleton_read_access_close (first_access);
+    }
+
+    {
+        kan_repository_singleton_write_access_t first_access =
+            kan_repository_singleton_write_query_execute (&first_write_from_child);
+        kan_repository_singleton_write_access_t second_access =
+            kan_repository_singleton_write_query_execute (&second_write_from_child);
+
+        struct first_singleton_t *first_singleton =
+            (struct first_singleton_t *) kan_repository_singleton_write_access_resolve (first_access);
+        KAN_TEST_ASSERT (first_singleton)
+
+        struct second_singleton_t *second_singleton =
+            (struct second_singleton_t *) kan_repository_singleton_write_access_resolve (second_access);
+        KAN_TEST_ASSERT (second_singleton)
+
+        first_singleton->x = 1u;
+        first_singleton->y = 2u;
+        first_singleton->observable_z = 3u;
+
+        second_singleton->a = 42u;
+        second_singleton->b = 13u;
+
+        kan_repository_singleton_write_access_close (first_access);
+        kan_repository_singleton_write_access_close (second_access);
+    }
+
+    {
+        kan_repository_singleton_read_access_t first_access =
+            kan_repository_singleton_read_query_execute (&first_read_from_root);
+
+        const struct first_singleton_t *singleton =
+            (struct first_singleton_t *) kan_repository_singleton_read_access_resolve (first_access);
+
+        KAN_TEST_ASSERT (singleton)
+        KAN_TEST_CHECK (singleton->x == 1u)
+        KAN_TEST_CHECK (singleton->y == 2u)
+        KAN_TEST_CHECK (singleton->observable_z == 3u)
+
+        kan_repository_singleton_read_access_close (first_access);
+    }
+
+    {
+        kan_repository_singleton_write_access_t second_access =
+            kan_repository_singleton_write_query_execute (&second_write_from_child);
+
+        struct second_singleton_t *singleton =
+            (struct second_singleton_t *) kan_repository_singleton_write_access_resolve (second_access);
+
+        KAN_TEST_ASSERT (singleton)
+        KAN_TEST_CHECK (singleton->a == 42u)
+        KAN_TEST_CHECK (singleton->b == 13u)
+
+        kan_repository_singleton_write_access_close (second_access);
+    }
+
+    kan_repository_enter_planning_mode (root_repository);
+    kan_repository_singleton_read_query_shutdown (&first_read_from_root);
+    kan_repository_singleton_write_query_shutdown (&first_write_from_child);
+    kan_repository_singleton_write_query_shutdown (&second_write_from_child);
+
+    kan_repository_destroy (root_repository);
+    kan_reflection_registry_destroy (registry);
+}
+
+KAN_TEST_CASE (singleton_write_events)
+{
+    kan_reflection_registry_t registry = kan_reflection_registry_create ();
+    KAN_REFLECTION_UNIT_REGISTRAR_NAME (repository) (registry);
+    KAN_REFLECTION_UNIT_REGISTRAR_NAME (test_repository) (registry);
+    kan_repository_t repository = kan_repository_create_root (KAN_ALLOCATION_GROUP_IGNORE, registry);
+
+    kan_repository_singleton_storage_t first_storage =
+        kan_repository_singleton_storage_open (repository, "first_singleton_t");
+
+    struct kan_repository_singleton_write_query_t write_singleton;
+    kan_repository_singleton_write_query_init (&write_singleton, first_storage);
+
+    kan_repository_event_storage_t event_z_storage =
+        kan_repository_event_storage_open (repository, "first_singleton_z_changed_event_t");
+
+    kan_repository_event_storage_t event_a_b_storage =
+        kan_repository_event_storage_open (repository, "first_singleton_a_b_changed_event_t");
+
+    struct kan_repository_event_fetch_query_t fetch_z_changed;
+    kan_repository_event_fetch_query_init (&fetch_z_changed, event_z_storage);
+
+    struct kan_repository_event_fetch_query_t fetch_a_b_changed;
+    kan_repository_event_fetch_query_init (&fetch_a_b_changed, event_a_b_storage);
+
+    kan_repository_enter_serving_mode (repository);
+    check_no_event (&fetch_z_changed);
+    check_no_event (&fetch_a_b_changed);
+
+    // Change Z and check events.
+    {
+        kan_repository_singleton_write_access_t access =
+            kan_repository_singleton_write_query_execute (&write_singleton);
+
+        struct first_singleton_t *singleton =
+            (struct first_singleton_t *) kan_repository_singleton_write_access_resolve (access);
+
+        KAN_TEST_ASSERT (singleton)
+        singleton->observable_z = 42u;
+        kan_repository_singleton_write_access_close (access);
+    }
+
+    check_z_changed_event (&fetch_z_changed, (struct first_singleton_z_changed_event_t) {.old_z = 0u, .new_z = 42u});
+    check_no_event (&fetch_a_b_changed);
+
+    // Set same value to Z and check that there is no event.
+    {
+        kan_repository_singleton_write_access_t access =
+            kan_repository_singleton_write_query_execute (&write_singleton);
+
+        struct first_singleton_t *singleton =
+            (struct first_singleton_t *) kan_repository_singleton_write_access_resolve (access);
+
+        KAN_TEST_ASSERT (singleton)
+        singleton->observable_z = 13u;
+        singleton->observable_z = 42u;
+        kan_repository_singleton_write_access_close (access);
+    }
+
+    check_no_event (&fetch_z_changed);
+    check_no_event (&fetch_a_b_changed);
+
+    // Set value to only A and check event.
+    {
+        kan_repository_singleton_write_access_t access =
+            kan_repository_singleton_write_query_execute (&write_singleton);
+
+        struct first_singleton_t *singleton =
+            (struct first_singleton_t *) kan_repository_singleton_write_access_resolve (access);
+
+        KAN_TEST_ASSERT (singleton)
+        singleton->observable_a = 1.0f;
+        kan_repository_singleton_write_access_close (access);
+    }
+
+    check_no_event (&fetch_z_changed);
+    check_a_b_changed_event (&fetch_a_b_changed, (struct first_singleton_a_b_changed_event_t) {
+                                                     .old_a = 0.0f, .old_b = 0.0f, .new_a = 1.0f, .new_b = 0.0f});
+
+    // Set value to only B and check event.
+    {
+        kan_repository_singleton_write_access_t access =
+            kan_repository_singleton_write_query_execute (&write_singleton);
+
+        struct first_singleton_t *singleton =
+            (struct first_singleton_t *) kan_repository_singleton_write_access_resolve (access);
+
+        KAN_TEST_ASSERT (singleton)
+        singleton->observable_b = 2.0f;
+        kan_repository_singleton_write_access_close (access);
+    }
+
+    check_no_event (&fetch_z_changed);
+    check_a_b_changed_event (&fetch_a_b_changed, (struct first_singleton_a_b_changed_event_t) {
+                                                     .old_a = 1.0f, .old_b = 0.0f, .new_a = 1.0f, .new_b = 2.0f});
+
+    // Set value to both A and B and check event.
+    {
+        kan_repository_singleton_write_access_t access =
+            kan_repository_singleton_write_query_execute (&write_singleton);
+
+        struct first_singleton_t *singleton =
+            (struct first_singleton_t *) kan_repository_singleton_write_access_resolve (access);
+
+        KAN_TEST_ASSERT (singleton)
+        singleton->observable_a = 4.0f;
+        singleton->observable_b = 3.0f;
+        kan_repository_singleton_write_access_close (access);
+    }
+
+    check_no_event (&fetch_z_changed);
+    check_a_b_changed_event (&fetch_a_b_changed, (struct first_singleton_a_b_changed_event_t) {
+                                                     .old_a = 1.0f, .old_b = 2.0f, .new_a = 4.0f, .new_b = 3.0f});
+
+    kan_repository_enter_planning_mode (repository);
+    kan_repository_singleton_write_query_shutdown (&write_singleton);
+    kan_repository_event_fetch_query_shutdown (&fetch_z_changed);
+    kan_repository_event_fetch_query_shutdown (&fetch_a_b_changed);
 
     kan_repository_destroy (repository);
     kan_reflection_registry_destroy (registry);
