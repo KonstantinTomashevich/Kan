ad7cf86e567acdc9780c549dccee2bbb6b8422a4
KonstantinTomashevich <konstantin.tomashevich@gmail.com>
2024-03-11 20:17:01 +0300

    #17. file_system: Add platform-dependant methods and implement them for Linux.

---
+++ test/file_system/test_file_system.c
@@ -0,0 +1,99 @@
+#include <string.h>
+
+#include <kan/file_system/entry.h>
+#include <kan/file_system/stream.h>
+#include <kan/testing/testing.h>
+
+static kan_bool_t write_text_file (const char *file, const char *content)
+{
+    struct kan_stream_t *stream = kan_direct_file_stream_open_for_write (file, KAN_TRUE);
+    if (!stream || !kan_stream_is_writeable (stream))
+    {
+        return KAN_FALSE;
+    }
+
+    const uint64_t content_length = strlen (content);
+    const kan_bool_t result = stream->operations->write (stream, strlen (content), content) == content_length;
+
+    kan_direct_file_stream_close (stream);
+    KAN_TEST_CHECK (kan_file_system_check_existence (file))
+    return result;
+}
+
+KAN_TEST_CASE (create_and_remove_empty_directory)
+{
+    KAN_TEST_CHECK (kan_file_system_make_directory ("test_directory"));
+    KAN_TEST_CHECK (kan_file_system_remove_empty_directory ("test_directory"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test_directory"))
+}
+
+KAN_TEST_CASE (create_and_remove_two_level_directories)
+{
+    KAN_TEST_CHECK (kan_file_system_make_directory ("test1"));
+    KAN_TEST_CHECK (kan_file_system_make_directory ("test1/test2"))
+    KAN_TEST_CHECK (kan_file_system_remove_empty_directory ("test1/test2"))
+    KAN_TEST_CHECK (kan_file_system_remove_empty_directory ("test1"));
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test1/test2"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test1"))
+}
+
+KAN_TEST_CASE (create_and_remove_file)
+{
+    KAN_TEST_CHECK (write_text_file ("test.txt", "Hello, world!"));
+    KAN_TEST_CHECK (kan_file_system_remove_file ("test.txt"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test.txt"))
+}
+
+KAN_TEST_CASE (create_and_remove_directory_with_files)
+{
+    KAN_TEST_CHECK (kan_file_system_make_directory ("test_directory"));
+    KAN_TEST_CHECK (write_text_file ("test_directory/test1.txt", "Hello, world!"))
+    KAN_TEST_CHECK (write_text_file ("test_directory/test2.txt", "Hello, world!"))
+    KAN_TEST_CHECK (kan_file_system_remove_directory_with_content ("test_directory"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test_directory/test1.txt"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test_directory/test2.txt"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test_directory"))
+}
+
+KAN_TEST_CASE (create_and_remove_hierarchy_with_files)
+{
+    KAN_TEST_CHECK (kan_file_system_make_directory ("test_directory"));
+    KAN_TEST_CHECK (write_text_file ("test_directory/test1.txt", "Hello, world!"))
+    KAN_TEST_CHECK (write_text_file ("test_directory/test2.txt", "Hello, world!"))
+    KAN_TEST_CHECK (kan_file_system_make_directory ("test_directory/subdirectory"));
+    KAN_TEST_CHECK (kan_file_system_make_directory ("test_directory/subdirectory/another"));
+    KAN_TEST_CHECK (write_text_file ("test_directory/subdirectory/another/log.log", "Hello, world!"))
+    KAN_TEST_CHECK (kan_file_system_remove_directory_with_content ("test_directory"))
+
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test_directory/subdirectory/another/log.log"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test_directory/subdirectory/another"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test_directory/subdirectory"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test_directory/test1.txt"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test_directory/test2.txt"))
+    KAN_TEST_CHECK (!kan_file_system_check_existence ("test_directory"))
+}
+
+KAN_TEST_CASE (query_status)
+{
+    KAN_TEST_CHECK (kan_file_system_make_directory ("test_directory"));
+    KAN_TEST_CHECK (write_text_file ("test_directory/test1.txt", "Hello, world!"))
+    KAN_TEST_CHECK (write_text_file ("test_directory/test2.txt", "Hello, world!!"))
+
+    struct kan_file_system_entry_status_t status;
+    KAN_TEST_CHECK (kan_file_system_query_entry ("test_directory/test1.txt", &status))
+    KAN_TEST_CHECK (status.type == KAN_FILE_SYSTEM_ENTRY_TYPE_FILE)
+    KAN_TEST_CHECK (status.size == 13u)
+    KAN_TEST_CHECK (!status.read_only)
+    uint64_t test1_last_modification_time_ns = status.last_modification_time_ns;
+
+    KAN_TEST_CHECK (kan_file_system_query_entry ("test_directory/test2.txt", &status))
+    KAN_TEST_CHECK (status.type == KAN_FILE_SYSTEM_ENTRY_TYPE_FILE)
+    KAN_TEST_CHECK (status.size == 14u)
+    KAN_TEST_CHECK (!status.read_only)
+    KAN_TEST_CHECK (test1_last_modification_time_ns <= status.last_modification_time_ns)
+
+    KAN_TEST_CHECK (kan_file_system_query_entry ("test_directory", &status))
+    KAN_TEST_CHECK (status.type == KAN_FILE_SYSTEM_ENTRY_TYPE_DIRECTORY)
+
+    KAN_TEST_CHECK (kan_file_system_remove_directory_with_content ("test_directory"))
+}
---
+++ unit/file_system/kan/file_system/path_container.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <string.h>
+
+#include <kan/api_common/bool.h>
+#include <kan/api_common/c_header.h>
+#include <kan/api_common/min_max.h>
+
+/// \file
+/// \brief Contains inline implementation for file system entry path container and operations on it.
+///
+/// \par Path container
+/// \parblock
+/// Path container is a buffer of maximum path length that is used to store path to file system entry. Its main goal
+/// is to provide operations that make it possible to avoid unnecessary allocations during file system lookup.
+/// \endparblock
+
+KAN_C_HEADER_BEGIN
+
+/// \brief Contains buffer for file system entry path.
+struct kan_file_system_path_container_t
+{
+    uint64_t length;
+    char path[KAN_FILE_SYSTEM_MAX_PATH_LENGTH];
+};
+
+/// \brief Resets path container length to given value and places null terminator at appropriate position.
+static inline void kan_file_system_path_container_reset_length (struct kan_file_system_path_container_t *container,
+                                                                uint64_t new_length)
+{
+    container->length = KAN_MIN (KAN_FILE_SYSTEM_MAX_PATH_LENGTH - 1u, new_length);
+    container->path[container->length] = '\0';
+}
+
+/// \brief Initializes path container by copying path from given string.
+static inline void kan_file_system_path_container_copy_string (
+    struct kan_file_system_path_container_t *target_container, const char *arbitrary_path)
+{
+    const uint64_t arbitrary_path_length = strlen (arbitrary_path);
+    kan_file_system_path_container_reset_length (target_container, arbitrary_path_length);
+
+    if (target_container->length > 0u)
+    {
+        memcpy (target_container->path, arbitrary_path, target_container->length);
+    }
+}
+
+/// \brief Initializes path container by copying data from another path container.
+static inline void kan_file_system_path_container_copy (struct kan_file_system_path_container_t *target_container,
+                                                        const struct kan_file_system_path_container_t *source_container)
+{
+    target_container->length = source_container->length;
+    memcpy (target_container->path, source_container->path, target_container->length + 1u);
+}
+
+/// \brief Appends given sub path to path container, automatically adding separator between base path and sub path.
+static inline void kan_file_system_path_container_append (struct kan_file_system_path_container_t *target_container,
+                                                          const char *sub_path)
+{
+    const uint64_t sub_path_length = strlen (sub_path);
+    const uint64_t new_length = target_container->length + 1u + sub_path_length;
+    const uint64_t slash_position = target_container->length;
+    const uint64_t sub_path_position = slash_position + 1u;
+    kan_file_system_path_container_reset_length (target_container, new_length);
+
+    if (slash_position < target_container->length)
+    {
+        target_container->path[slash_position] = '/';
+    }
+
+    if (sub_path_position < target_container->length)
+    {
+        const uint64_t to_copy = target_container->length - sub_path_position;
+        memcpy (&target_container->path[sub_path_position], sub_path, to_copy);
+    }
+}
+
+/// \brief Appends given raw string to path container.
+static inline void kan_file_system_path_container_add_suffix (struct kan_file_system_path_container_t *target_container,
+                                                              const char *suffix)
+{
+    const uint64_t suffix_length = strlen (suffix);
+    const uint64_t new_length = target_container->length + suffix_length;
+    const uint64_t suffix_position = target_container->length;
+    kan_file_system_path_container_reset_length (target_container, new_length);
+
+    if (suffix_position < target_container->length)
+    {
+        const uint64_t to_copy = target_container->length - suffix_position;
+        memcpy (&target_container->path[suffix_position], suffix, to_copy);
+    }
+}
+
+KAN_C_HEADER_END
