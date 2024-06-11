6e338353a86ff357808b09a711bc760a138817c1
KonstantinTomashevich <konstantin.tomashevich@gmail.com>
2024-06-11 20:34:53 +0300

    #35. Disable leak detection in address sanitizer for applications with SDL launchers as it seems that leak detection bugs out with SDL.

--- unit/application_framework_static_launcher_sdl/kan/application_framework_static_launcher/main.c
+++ unit/application_framework_static_launcher_sdl/kan/application_framework_static_launcher/main.c
@@ -5,6 +5,11 @@
 extern const char *core_configuration_path;
 extern const char *program_configuration_path;
 
+const char *__asan_default_options (void)
+{
+    return "detect_leaks=0";
+}
+
 int main (int argc, char *argv[])
 {
     return kan_application_framework_run (core_configuration_path, program_configuration_path, (uint64_t) argc, argv);
