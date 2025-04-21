498670505f5f233f3aca643f820a67e46326ecbb
Konstantin Tomashevich <konstantin.tomashevich@gmail.com>
2025-04-21 23:59:30 +0300

    #76. Make it possible to skip asan leaks test in tests and enable graphics tests in sanitizer.

--- cmake/kan/test_runner_ecosystem/test_runner.c
+++ cmake/kan/test_runner_ecosystem/test_runner.c
@@ -3,6 +3,13 @@
 #include <kan/error/context.h>
 #include <kan/testing/testing.h>
 
+#if defined(KAN_TEST_ECOSYSTEM_NO_ASAN_LEAK_TEST)
+const char *__asan_default_options (void)
+{
+    return "detect_leaks=0";
+}
+#endif
+
 extern void execute_test_case_${TEST_NAME} (void);
 
 int main (int argc, char *argv[])
